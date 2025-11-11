690c30394bcfb66850657d6567858fc0d8dc6a49
Lumpi78 <Lutz.Thieme@gmx.de>
2025-11-11 13:36:05 +0100

    Initial commit

---
+++ hello-k8s/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Standard-Endpunkt
+app.get('/', (req, res) => {
+  res.send('Hello Kubernetes!');
+});
+
+// /health Endpunkt 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
