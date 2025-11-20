81796f736d881ab4569ddf7c5e5e4470b7b94014
Lumpi78 <Lutz.Thieme@gmx.de>
2025-11-20 09:38:29 +0100

    Test: Trigger CI workflow

--- hello-k8s/app.js
+++ hello-k8s/app.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 // Standard-Endpunkt
 app.get('/', (req, res) => {
-  res.send('Hello from GitHub Actions new2!');
+  res.send('Hello from GitHub Actions new3!');
 });
 
 // /health Endpunkt 
